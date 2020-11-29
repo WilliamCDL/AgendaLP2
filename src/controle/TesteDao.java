29e5d1ec1496fea6e9385ae2a56c85cca23e1336
WilliamCDL <42789470+WilliamCDL@users.noreply.github.com>
2020-11-29 00:36:02 -0300

    TesteCriaçãoNotaDao
    criado e adicionado uma nota do tipo evento, para verificar se havia algum erro de codigo.

---
+++ src/controle/TesteDao.java
@@ -0,0 +1,39 @@
+package controle;
+
+import java.util.Date;
+import java.util.Optional;
+
+import modelo.Dao;
+import modelo.Nota;
+import modelo.NotaDao;
+import modelo.NotaEvento;
+
+public class TesteDao {
+
+	private static Dao<Nota> notaDao;
+	
+	public static void main(String[] args) {
+		
+		 notaDao = new NotaDao();
+		// TODO Auto-generated method stub
+		 NotaEvento teste1 = new NotaEvento();
+		 teste1.setTitulo("evento1");
+		 Date data1 = new Date();
+		 teste1.setData(data1);
+		 
+		 notaDao.save(teste1);
+		 
+		 Optional<Nota> n = TesteDao.getUser(0);
+		 System.out.println(n.get().getTitulo());
+		 System.out.println(n.get().getData());
+		 
+		 
+	}
+	
+	private static Optional<Nota> getUser(long id) {
+        Optional<Nota> nota = notaDao.get(id);
+        
+        return nota;
+    }
+
+}
--- src/modelo/Nota.java
+++ src/modelo/Nota.java
@@ -8,7 +8,7 @@ public abstract class Nota {
 	protected Date data;
 	protected boolean pertoDoPrazo; 
 	protected boolean tarefaRealizada;
-	protected static int[] dias = {31,28,31,30,31,30,31,31,30,31,30,31};
+	protected int[] diaMes = {31,28,31,30,31,30,31,31,30,31,30,31};
 	
 	public String getTitulo() {
 		return titulo;
@@ -36,6 +36,39 @@ public abstract class Nota {
 	}
 	public void setTarefaRealizada(boolean tarefaRealizada) {
 		this.tarefaRealizada = tarefaRealizada;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public Date dataNotificar(int diasSomar) {
+		Date dataAviso = new Date();
+		int dia = dataAviso.getDate();
+		int mes = dataAviso.getMonth();
+		int ano = dataAviso.getYear();
+		
+		if(ano%100==0 & ano%400==0) {
+			diaMes[1]=29;
+		}else if (ano%4==0) {
+			diaMes[1]=29;
+		}
+		
+		if ((dia+diasSomar)>diaMes[mes]) {
+			dia=(dia+diasSomar)%diaMes[mes];
+			if(mes+1>11) {
+				mes=0;
+				ano++;
+			}else {
+				mes++;
+			}
+			
+		} else {
+			dia+=diasSomar;
+		}
+		
+		dataAviso.setDate(dia);
+		dataAviso.setMonth(mes);
+		dataAviso.setYear(ano);
+		
+		return dataAviso;
 	}
 	
 	public abstract void notificar();
